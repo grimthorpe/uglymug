2c5f319d335ce5c0684b2a3a28e14b15e2ea98aa
julian <julian>
2001-07-09 15:35:11 +0000

    newer - even if it is bollocks

--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define VERSION "[build #8 by julian at Sat Jul 7 12:28:53 BST 2001 on spoogle.uglymug.org.uk]"
+#define VERSION "[build #13 by julian at Mon Jul 9 16:27:03 BST 2001 on spoogle.uglymug.org.uk]"
