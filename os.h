822b54b06da4834179e7247d91d667f15f0d445a
ozzard <ozzard>
2003-04-18 21:39:33 +0000

    SRAND48

--- os.h
+++ os.h
@@ -7,28 +7,33 @@
 
 // Linux
 #ifdef linux
+#	define	HAS_FORK	1
+#	define	HAS_SIGNALS	1
 #	define	NEEDS_CRYPT_H	1
 #	define	NEEDS_RESOURCES	0
-#	define	USE_WINSOCK2	0
 #	define	USE_BSD_SOCKETS	1
+#	define	USE_WINSOCK2	0
 #endif /* linux */ 
 
-// Sun
+// Sun - typically Solaris these days.
 #if sun
-#	define NEEDS_CRYPT_H	1
-#	define	USE_WINSOCK2	0
+#	define	HAS_FORK	1
+#	define	HAS_SIGNALS	1
+#	define	NEEDS_CRYPT_H	1
 #	define	USE_BSD_SOCKETS	1
+#	define	USE_WINSOCK2	0
 #endif /* sun */ 
 
 // MinGW
 #ifdef __MINGW32__
 #	define	HAS_FORK	0
 #	define	HAS_SIGNALS	0
+#	define	HAS_SRAND48	0
 #	define	NEEDS_CRYPT_H	1
 #	define	NEEDS_GETOPT	1
 #	define	NEEDS_RESOURCES	0
-#	define	USE_WINSOCK2	1
 #	define	USE_BSD_SOCKETS	0
+#	define	USE_WINSOCK2	1
 #endif
 
 // Defaults - assume a UNIX of some kind.
@@ -37,6 +42,9 @@
 #endif
 #ifndef	HAS_SIGNALS
 #	define	HAS_SIGNALS	1
+#endif
+#ifndef	HAS_SRAND48
+#	define	HAS_SRAND48	1
 #endif
 #ifndef	NEEDS_RESOURCES
 #	define	NEEDS_RESOURCES	1
