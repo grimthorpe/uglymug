9251d3aaf964f7f3d7eb17278dd1e192cfad3379
ozzard <ozzard>
2003-10-26 13:29:18 +0000

    Update comments for doxygen

--- predicates.c
+++ predicates.c
@@ -1,9 +1,6 @@
 /* static char SCCSid[] = "@(#)predicates.c	1.32\t10/17/95"; */
-/*
- * predicates.c: Predicates for testing conditions. This lot are
- *	independent of netmud.
- *
- *	Peter Crowther, 22/7/91.
+/** \file
+ * Predicates for testing conditions. This lot are independent of netmud and have been separated out into their own compilation unit as a result.
  */
 
 #include "copyright.h"
@@ -13,7 +10,7 @@
 #include "context.h"
 
 
-/* This defines what flags are allowed to be set on each object type.
+/** The flag map defines what flags are allowed to be set on each object type.
    Used as the first test in set and as a check in the sanity-check */
 
 unsigned char flag_map[][64] = 
@@ -117,6 +114,10 @@ unsigned char flag_map[][64] =
 };
 
 
+/**
+ * Return whether 'flag' is allowed to be set on an object of type 'type'.
+ */
+
 bool
 is_flag_allowed(
 typeof_type	type,
@@ -135,6 +136,11 @@ flag_type	flag)
 }
 
 
+/**
+ * Return whether the player identified in context c can, at least in theory, open an exit into 'where'.
+ * TODO: Work out what this is used for, as the logic is hopelessly out of date.  PJC, 2003-10-26.
+ */
+
 const bool
 can_link_to (
 const	context	&c,
@@ -147,6 +153,20 @@ const	dbref	where)
 	   && (c.controls_for_write (where)));
 }
 
+
+/**
+ * Can this context make changes to 'what'?  Rules:
+ *
+ * - what must be a legal dbref
+ * - what must not be readonly
+ * - At least one of the following conditions must hold:
+ * -- The effective ID of the player is GOD; or
+ * -- The player is really a Wizard and they're not trying to control another Wizard; or
+ * -- The player is effectively a Wizard and 'what' has no wizard flag set; or
+ * -- The player owns 'what'; or
+ * -- The player is part of a build group that owns 'what'; or
+ * -- The player owns what's controller.
+ */
 
 const bool
 context::controls_for_write (
@@ -164,6 +184,19 @@ const
 			|| (get_effective_id() == db [db [what].get_owner()].get_controller ())));	/* Players own their puppets and puppets' stuff */
 }
 
+
+/**
+ * Can this context see all properties of 'what'?  Rules:
+ *
+ * - what must be a legal dbref
+ * - At least one of the following conditions must hold:
+ * -- The context is a real Apprentice or Wizard;
+ * -- The context is an effective Apprentice or Wizard;
+ * -- The effective ID or the real ID of the context owns the object;
+ * -- The effective ID or the real ID of the context controls the object's owner;
+ * -- The context's build group owns the object;
+ * -- The object is set Visible.
+ */
 
 const bool
 context::controls_for_read (
@@ -186,6 +219,10 @@ const
 }
 
 
+/**
+ * TODO: Is this still used?  If so, it shouldn't be!  PJC, 2003-10-26
+ */
+
 const bool
 controls_for_read (
 const	dbref	/* real_who */,
@@ -202,6 +239,10 @@ const	dbref	effective_who)
 			|| (Apprentice(effective_who))));		/* Real and Effective APPRENTICES can read everything */
 }
 
+
+/**
+ * TODO: I have no idea what this does! PJC 2003-10-26.
+ */
 
 const bool
 can_link (
